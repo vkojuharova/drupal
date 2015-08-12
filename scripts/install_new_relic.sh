0aea81801dabc095b047cb89b4ca3253ea256325
Vanja Kojuharova <vanja_kojuharova@harvard.edu>
2015-08-12 12:25:23 -0400

    Add New Relic

--- scripts/install_new_relic.sh
+++ scripts/install_new_relic.sh
@@ -4,5 +4,5 @@ rpm -Uvh http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarc
 yum install -y newrelic-php5 || true
 
 export NR_INSTALL_SILENT=true
-export NR_INSTALL_KEY=your_new_relic_key_here
+export NR_INSTALL_KEY=5688e490fe520b8f585ec5160c8c0c8c75e28db0
 newrelic-install install
